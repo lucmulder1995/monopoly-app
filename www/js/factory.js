e9c2747fdb49382909fa8347161b4f39e69698f4
Luc Mulder <ldemulde@student.avans.nl>
2016-04-02 15:25:59 +0200

    commit

---
+++ www/js/factory.js
@@ -0,0 +1,62 @@
+// myApp.factory('user', function(){
+//     return {};
+// });
+//
+// myApp.factory('game', function(){
+//     return {};
+// });
+//
+// myApp.factory('user', function () {
+//
+//     var data = {
+//         'user': {}
+//     }
+//
+//     return {
+//         getUser: function () {
+//             return data.user;
+//         },
+//         setUser: function (user) {
+//             data.user = user;
+//         }
+//     };
+// });
+
+myApp.factory('dataStorage', function () {
+
+    var data;
+
+    if(window.localStorage['dataStorage'] != undefined){
+        data = JSON.parse(window.localStorage['dataStorage']);
+    }else{
+        data = {};
+    }
+
+    var saveData = function(){
+        window.localStorage['dataStorage'] = JSON.stringify(data);
+    }
+
+    return {
+        getGame: function () {
+            return data.game;
+        },
+        setGame: function (game) {
+            data.game = game;
+            saveData();
+        },
+        getUser: function () {
+            return data.user;
+        },
+        setUser: function (user) {
+            data.user = user;
+            saveData();
+        },
+        setCurrentSquare: function(square){
+            data.currentSquare = square;
+            saveData();
+        },
+        getCurrentSquare: function () {
+            return data.currentSquare;
+        },
+    };
+});
