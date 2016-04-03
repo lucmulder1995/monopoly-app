a546f8250bdf41475a7899f884c8ad9f5e85a1a7
lucmulder29 <ldemulde@student.avans.nl>
2016-04-03 18:23:22 +0200

    test

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -989,7 +989,8 @@ myApp.controller('LoginCtrl', function ($scope, dataStorage) {
                     }
 
                     console.log('no active game found, start new game');
-                    window.location.href = "#/app/startGame";
+                    window.open('#/app/startGame', '_self ', 'location=yes');
+                    //window.location.href = "#/app/startGame";
 
                 }
                 console.log(data);
